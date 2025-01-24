c9506d4627011f6edc660dd22b9a9c6a74a4e000
CHANDRASHEKHAR BADGUJAR <cbadgujar2050@gmail.com>
2025-01-24 15:47:53 +0530

    Add files via upload

---
+++ factnum.java
@@ -0,0 +1,16 @@
+package BASIC;
+
+public class factnum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int i,fact=1;
+       int num=10;
+       
+       for(i=1;i<=10;i++) {
+    	   fact=fact*i;
+       }
+       System.out.println("factorial of number="+fact);
+	}
+ 
+}
